2e4663fbde6f71bc0b4965cbc39664579b5637fb
Adlet <160103098@stu.sdu.edu.kz>
2020-04-19 23:05:12 +0600

    feat: add formula for p

---
+++ p.rb
@@ -0,0 +1,18 @@
+class P < Formula
+  desc "p - allows you to manage cocoa dependency managers (cocoapods, carthage, spm) in more robust & convenient way."
+  homepage "https://github.com/nugmanoff/p"
+  url "https://github.com/nugmanoff/p"
+  version ""
+  sha256 ""
+
+  depends_on "gawk" => :build
+  depends_on "coreutils" => :build
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  test do
+    system "false"
+  end
+end
