909ea486cdcd1758ab1b7f0e09ff7e04ca1ccde0
Adlet <160103098@stu.sdu.edu.kz>
2020-04-19 23:15:33 +0600

    fix: remove version

--- p.rb
+++ p.rb
@@ -2,8 +2,6 @@ class P < Formula
   desc "p - allows you to manage cocoa dependency managers (cocoapods, carthage, spm) in more robust & convenient way."
   homepage "https://github.com/nugmanoff/p"
   url "https://github.com/nugmanoff/p"
-  version ""
-  sha256 ""
 
   depends_on "gawk" => :build
   depends_on "coreutils" => :build
