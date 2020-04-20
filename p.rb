dd1a0bfa85b59c2d309d0e3b80ba06d057298d96
Adlet <160103098@stu.sdu.edu.kz>
2020-04-20 21:55:54 +0600

    fix: edit sha256

--- p.rb
+++ p.rb
@@ -2,7 +2,7 @@ class P < Formula
   desc "p - allows you to manage cocoa dependency managers (cocoapods, carthage, spm) in more robust & convenient way."
   homepage "https://github.com/nugmanoff/p"
   url "https://github.com/nugmanoff/p/archive/0.1.0.tar.gz"
-  sha256 "a07a3d59644efa27c4152e385567505f3abc6761"
+  sha256 "9d81905cfd9389343e2ac400e05d7d84755d36ca2fa5fbacc794b6daa2cff0de"
 
   depends_on "gawk" => :build
   depends_on "coreutils" => :build
