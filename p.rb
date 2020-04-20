f3403be5a230d3a0e1aad49426b4ad08f9bd66b6
Adlet <160103098@stu.sdu.edu.kz>
2020-04-20 21:47:37 +0600

    feat: add release url

--- p.rb
+++ p.rb
@@ -1,7 +1,8 @@
 class P < Formula
   desc "p - allows you to manage cocoa dependency managers (cocoapods, carthage, spm) in more robust & convenient way."
   homepage "https://github.com/nugmanoff/p"
-  url "https://github.com/nugmanoff/p"
+  url "https://github.com/nugmanoff/p/archive/0.1.0.tar.gz"
+  sha256 "a07a3d59644efa27c4152e385567505f3abc6761"
 
   depends_on "gawk" => :build
   depends_on "coreutils" => :build
